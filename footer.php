760eb1a72835f06fb67740f4722eec1883e40b2f
SanthoshPatkar <72272748+SanthoshPatkar@users.noreply.github.com>
2021-09-29 19:01:49 +0530

    footer page

---
+++ footer.php
@@ -0,0 +1,43 @@
+</main>
+
+<!-- footer -->
+
+<footer id="footer" class="bg-dark text-white py-5" style="margin-top: 2%;">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-5 col-12">
+        <h4 class="font-rubik font-size-20">Expo shop</h4>
+        <p class="font-size-14 font-rale text-white-50">Start your Golden shopping</p>
+      </div>
+      <div class="col-lg-5 col-12">
+        <h4 class="font-rubik font-size-20">Quick Links</h4>
+        <div class="d-flex flex-column flex-wrap">
+          <a href="index.php" class="font-rale font-size-16 text-white-50 pb-1">Home</a>
+          <a href="laptop.php" class="font-rale font-size-16 text-white-50 pb-1">Laptop</a>
+          <a href="mobile.php" class="font-rale font-size-16 text-white-50 pb-1">Mobiles</a>
+          <a href="Tv.php" class="font-rale font-size-16 text-white-50 pb-1">Telivision</a>
+        </div>
+      </div>
+      <div class="col-lg-2 col-12">
+        <h4 class="font-rubik font-size-20">Info-Detail</h4>
+        <div class="d-flex flex-column flex-wrap">
+          <a href="cart.php" class="font-rale font-size-14 text-white-50 pb-1">Cart</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <p style="text-align: center; padding-top:10px">@santhosh G Patkar</p>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
+  crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"
+  integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
+  crossorigin="anonymous" referrerpolicy="no-referrer"></script> -->
+<script src="index.js"></script>
+</body>
+
+</html>
