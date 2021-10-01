6750a351d50b486f1ea0839ca427d140e8857830
SanthoshPatkar <72272748+SanthoshPatkar@users.noreply.github.com>
2021-10-01 18:53:18 +0530

    confirm page

---
+++ confirm.php
@@ -0,0 +1,12 @@
+<?php
+include('header.php');
+?>
+<div class="text-success" style=' height:300px; display:flex; justify-content:center; align-items:center;'>
+<h1>Product is Sucessfully Ordered</h1>
+</div>
+<h3 style='display:flex; justify-content:center; align-items:center;'> We will Contact you shortly</h3>
+<a style='display:flex; justify-content:center; align-items:center;' href="index.php" >Back to home</a>
+<?php
+
+include('footer.php');
+?>
