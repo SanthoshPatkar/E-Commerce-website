83dfdf8020c427ae5b0dd9c611a3f27b7caf26c0
SanthoshPatkar <ssanthoshp159@gmail.com>
2021-10-15 23:12:45 +0530

    cart file

---
+++ template/cart_not.php
@@ -0,0 +1,12 @@
+<section id="cart" class="py-3 mb-5">
+    <div class="container-fluid w-75">
+        <h5 class="font-baloo font-size-20">Shopping Cart</h5>
+        <div class="row" style="height:300px">
+            <div class="col-sm-9">
+                <div class="col-sm-12">
+                    <h1 class="mt-5 py-3 font-size-60 text-center">Empty Cart</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
